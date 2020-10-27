0e2fbdf64bab7aa1c57098906da83e71f7bb4d0e
2022jobyrne <22jobyrne@athenian.org>
2020-10-27 19:40:21 +0000

    code

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,21 @@
-public class Person {
-    
+public class Person 
+{
+
+    public String firstName;
+    public String lastName;
+    public Person(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public void doSomething() 
+    {
+        System.out.println("I am watching netflix");
+    }        
+    public String toString()
+    {
+        return (firstName + " " + lastName);
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,8 @@
 public class PersonClient {
     public static void main(String[] args) {
+        Person p1 = new Person("Jane", "Smith");
+        p1.doSomething();
+        System.out.println(p1);
         
     }
 }
